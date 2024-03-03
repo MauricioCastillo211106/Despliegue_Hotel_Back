21e01329f590a5a1857e50c1895b1bd89d5cfca3
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2024-03-02 20:25:18 -0600

    cambios

--- database/sequelize.js
+++ database/sequelize.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
 // adapters/persistence/sequelize.ts
 const sequelize_1 = require("sequelize");
-exports.sequelize = new sequelize_1.Sequelize('hex_demo', 'postgres', 'POSTGRES', {
-    host: 'localhost',
+exports.sequelize = new sequelize_1.Sequelize('hex_demo', 'postgres', 'Hotel123', {
+    host: 'hotel-mantenimiento.cp2u8yo0qyl1.us-east-1.rds.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
 });
--- database/sequelize.ts
+++ database/sequelize.ts
@@ -1,8 +1,8 @@
 // adapters/persistence/sequelize.ts
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('hex_demo', 'postgres', '040902', {
-    host: 'localhost',
+export const sequelize = new Sequelize('hex_demo', 'postgres', 'Hotel123', {
+    host: 'hotel-mantenimiento.cp2u8yo0qyl1.us-east-1.rds.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
 });
